48910526d672c1b833738987089d81815dd34715
Deepak Jayaram <dpk@Deepaks-MacBook-Pro.local>
2020-03-07 13:39:33 -0500

    Cleaning up stuff left over from the previous commit, basic setup - adding hooks to fetchData, displaying image and other attributes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <span>Hello World</span>
-        </div>
-    );
-}
+const App = () => {
+    const [hasError, setErrors] = useState(false);
+    const [cardData, setCards] = useState([]);
+
+    async function fetchData() {
+        const res = await fetch(
+            'https://api.elderscrollslegends.io/v1/cards?pageSize=20'
+        );
+        res.json()
+            .then(res => setCards(res))
+            .catch(err => setErrors(err));
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const { cards = [] } = cardData;
+
+    if (!cards.length) {
+        return <span>Loading...</span>;
+    }
+
+    return cards.map(item => {
+        const {
+            id,
+            imageUrl,
+            name,
+            text,
+            type,
+            set: { name: setName }
+        } = item;
+        return (
+            <div key={id}>
+                <img src={imageUrl} alt={`${name}`} />
+                <h2>{name}</h2>
+                <p>{text}</p>
+                <h3>{type}</h3>
+                <h3>{setName}</h3>
+            </div>
+        );
+    });
+};
 
 export default App;
